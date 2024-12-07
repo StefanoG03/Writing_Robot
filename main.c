9d6af08c198fbd9b082fb70bc867b2fae67b9951
Stefano Geymonat <egysg11@nottingham.ac.uk>
2024-12-07 19:44:12 +0000

    added most flowchart and SystemManualTemplate24 and simplified main code

--- main.c
+++ main.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// #include <conio.h>
-// #include <windows.h>
 #include "rs232.h"
 #include "serial.h"
 
-#define bdrate 115200  /* 115200 baud */
-#define LINE_WIDTH 100 // Width of each line
+#define BAUD_RATE 115200 // Communication baud rate
+#define LINE_WIDTH 100   // Width of each line for text placement
+#define SCALE_MIN 4      // Minimum allowed scaling factor
+#define SCALE_MAX 10     // Maximum allowed scaling factor
+#define LINE_COUNT 1027  // Number of lines in the font data file
+#define CHAR_WIDTH 18.0F // Width of each character in the font
+#define LINE_SPACING -5  // Vertical spacing between lines
 
+// Function to send commands to the robot
 void SendCommands(char *buffer);
 
-#define LINE_COUNT 1027
-
-// Define struct to hold data from each line
+// Struct to hold font data for each character
 typedef struct
 {
     float Xposition;
@@ -21,220 +23,180 @@ typedef struct
     int Zposition;
 } DataEntry;
 
+// Function to get a valid scaling factor from the user
 float get_scale_factor()
 {
     float scale;
-    int cha;
-
-    do
+    int ch; // Used to clear the input buffer
+    while (1)
     {
-        printf("Enter a scaling factor between 4 and 10: ");
-        if (scanf("%f", &scale) != 1)
-        { // Check if the input is not a float
-            printf("Invalid input! Please enter a value between 4 and 10.\n");
-
-            // Clear the buffer to remove invalid input
-            while ((cha = getchar()) != '\n' && cha != EOF)
-                ;
+        printf("Enter a scaling factor between %d and %d: ", SCALE_MIN, SCALE_MAX);
+        if (scanf("%f", &scale) == 1 && scale >= SCALE_MIN && scale <= SCALE_MAX)
+        {
+            return scale / CHAR_WIDTH; // Scale factor for character width adjustment
         }
-        else if (scale < 4 || scale > 10)
-        { // Check if the number is out of range
-            printf("Invalid input! Please enter a value between 4 and 10.\n");
-        }
-    } while (scale < 4 || scale > 10);
-
-    return scale / 18;
+        printf("Invalid input! Please enter a value between %d and %d.\n", SCALE_MIN, SCALE_MAX);
+        while ((ch = getchar()) != '\n' && ch != EOF)
+            ; // Clear input buffer
+    }
 }
+
 // Function to calculate the width of a word
-float word_width(const char *word, float scaleFactor)
+float calculate_word_width(const char *word, float scaleFactor)
 {
-    int length = (int)strlen(word);             // Calculate the length of the word
-    return (float)length * 18.0F * scaleFactor; // Width calculation
+    return (float)strlen(word) * CHAR_WIDTH * scaleFactor; // Width based on scaled character width
 }
-// Function to check if the word fits in the remaining space on the line
-int space_remaining(double *remaining_space, double word_width)
+
+// Function to check if a word fits in the remaining line space
+int fits_in_line(float *remaining_space, float word_width)
 {
     if (*remaining_space >= word_width)
     {
-        *remaining_space -= word_width; // Subtract word width from remaining space
-
-        return 1; // Word fits
+        *remaining_space -= word_width; // Update remaining space
+        return 1;                       // Word fits
     }
-    else
-    {
-        return 0; // Word doesn't fit
-    }
+    return 0; // Word doesn't fit
 }
-// Function to open a file and return the file pointer
+
+// Function to open a file and handle errors
 FILE *open_file(const char *filename)
 {
     FILE *file = fopen(filename, "r");
-    if (file == NULL)
+    if (!file)
     {
         printf("Error opening file: %s\n", filename);
     }
     return file;
 }
-// Function to locate character's stroke data
-DataEntry *find_character_data(char character, DataEntry *SingleStrokeData, int *stroke_count)
+
+// Function to find the stroke data for a specific character
+DataEntry *find_character_data(char character, DataEntry *fontData, int *stroke_count)
 {
-    int ascii_val = (int)character;
+    int ascii_val = (int)character; // Get ASCII value of the character
     for (int i = 0; i < LINE_COUNT; i++)
     {
-        if (SingleStrokeData[i].Xposition == 999 && SingleStrokeData[i].Yposition == ascii_val)
+        if (fontData[i].Xposition == 999 && fontData[i].Yposition == ascii_val)
         {
-            *stroke_count = SingleStrokeData[i].Zposition; // Number of strokes for this character
-            return &SingleStrokeData[i + 1];               // Return pointer to the stroke data start
+            *stroke_count = fontData[i].Zposition; // Get stroke count
+            return &fontData[i + 1];               // Return pointer to the first stroke data
         }
     }
-    return NULL; // Character not found
+    return NULL; // Character data not found
 }
-// Function to convert stroke data to G-code commands for a word
-void convert_word_to_gcode(const char *word, DataEntry *SingleStrokeData, float scaleFactor, float *current_Xpos, float current_Ypos)
+
+// Function to generate G-code commands for a word
+void generate_gcode_for_word(const char *word, DataEntry *fontData, float scaleFactor, float *current_Xpos, float current_Ypos)
 {
-    char buffer[4000] = ""; // Buffer to hold G-code commands for the entire word
-
-    // Process each character in the word
-    for (int i = 0; word[i] != '\0'; i++)
-    {
+    char buffer[4000] = ""; // Buffer for G-code commands
+    for (int i = 0; word[i]; i++)
+    { // Process each character in the word
         int stroke_count;
-        DataEntry *charData = find_character_data(word[i], SingleStrokeData, &stroke_count);
-
-        if (charData != NULL)
+        DataEntry *charData = find_character_data(word[i], fontData, &stroke_count);
+        if (charData)
         {
             for (int j = 0; j < stroke_count; j++)
-            {
+            { // Generate G-code for each stroke
                 float scaledX = (charData[j].Xposition * scaleFactor) + *current_Xpos;
                 float scaledY = (charData[j].Yposition * scaleFactor) + current_Ypos;
-
-                if (charData[j].Zposition == 0)
-                {
-                    sprintf(buffer, "S0\nG0 X%.2f Y%.2f\n", scaledX, scaledY); // Pen up
-                }
-                else
-                {
-                    sprintf(buffer, "S1000\nG1 X%.2f Y%.2f\n", scaledX, scaledY); // Pen down
-                }
-                SendCommands(buffer);
+                sprintf(buffer, "S%d\nG%c X%.2f Y%.2f\n",
+                        charData[j].Zposition ? 1000 : 0,  // Pen state
+                        charData[j].Zposition ? '1' : '0', // Move type
+                        scaledX, scaledY);
+                SendCommands(buffer); // Send G-code command
             }
         }
         else
         {
-            printf("Character: %c - Stroke data not found.\n", word[i]);
+            printf("Character '%c' - Stroke data not found.\n", word[i]);
         }
-
-        *current_Xpos += 18 * scaleFactor; // Increment X-position for the next character
+        *current_Xpos += CHAR_WIDTH * scaleFactor; // Advance to next character position
     }
 }
+
+// Function to reset position for a new line
+void reset_position(float *current_Xpos, float *current_Ypos, float scaleFactor, double *remaining_space)
+{
+    *current_Xpos = 0;                                        // Reset X-position
+    *current_Ypos += LINE_SPACING - CHAR_WIDTH * scaleFactor; // Move to the next line
+    *remaining_space = LINE_WIDTH;                            // Reset remaining space for the new line
+    char buffer[100];
+    sprintf(buffer, "G0 X%.2f Y%.2f\n", *current_Xpos, *current_Ypos);
+    SendCommands(buffer); // Send command to move to the new line
+}
+
 int main()
 {
-    // char mode[]= {'8','N','1',0};
-    char buffer[100];
-
-    // If we cannot open the port then give up immediately
     if (CanRS232PortBeOpened() == -1)
     {
-        printf("\nUnable to open the COM port (specified in serial.h) ");
-        exit(0);
-    }
-
-    // Time to wake up the robot
-    printf("\nAbout to wake up the robot\n");
-
-    // We do this by sending a new-line
-    sprintf(buffer, "\n");
-    // printf ("Buffer to send: %s", buffer); // For diagnostic purposes only, normally comment out
-    PrintBuffer(&buffer[0]);
-    Sleep(100);
-
-    // This is a special case - we wait  until we see a dollar ($)
-    WaitForDollar();
-
-    printf("\nThe robot is now ready to draw\n");
-
-    // These commands get the robot into 'ready to draw mode' and need to be sent before any writing commands
-    sprintf(buffer, "G1 X0 Y0 F1000\n");
-    SendCommands(buffer);
-    sprintf(buffer, "M3\n");
-    SendCommands(buffer);
-    sprintf(buffer, "S0\n");
-    SendCommands(buffer);
-
-    // Open the font data file
-    FILE *file = fopen("SingleStrokeFont.txt", "r");
-    if (file == NULL)
-    {
-        printf("Error opening file.\n");
+        printf("Unable to open the COM port (specified in serial.h).\n");
         return 1;
     }
-    // Load font data into an array of structs
-    DataEntry SingleStrokeData[LINE_COUNT];
+
+    printf("Initializing robot...\n");
+    SendCommands("\n"); // Wake up robot
+    PrintBuffer("\n");
+    Sleep(100);
+    WaitForDollar(); // Wait for the robot to signal readiness
+    printf("Robot ready to draw.\n");
+
+    // Set initial robot state
+    SendCommands("G1 X0 Y0 F1000\n");
+    SendCommands("M3\n");
+    SendCommands("S0\n");
+
+    // Load font data
+    FILE *fontFile = open_file("SingleStrokeFont.txt");
+    if (!fontFile)
+        return 1;
+
+    DataEntry fontData[LINE_COUNT];
     for (int i = 0; i < LINE_COUNT; i++)
     {
-        fscanf(file, "%f %f %d", &SingleStrokeData[i].Xposition, &SingleStrokeData[i].Yposition, &SingleStrokeData[i].Zposition);
+        fscanf(fontFile, "%f %f %d", &fontData[i].Xposition, &fontData[i].Yposition, &fontData[i].Zposition);
     }
-    fclose(file);
+    fclose(fontFile);
 
-    // Get scaling factor from the user
+    // Get scale factor from user
     float scaleFactor = get_scale_factor();
     printf("Scale factor: %f\n", scaleFactor);
 
-    // Prompt for and open the input text file
-    char filename[200];
+    // Open input file for text
+    char inputFilename[200];
     printf("Enter the name of the text file: ");
-    scanf("%199s", filename);
-
-    FILE *file2 = open_file(filename);
-    if (file2 == NULL)
-    {
+    scanf("%199s", inputFilename);
+    FILE *inputFile = open_file(inputFilename);
+    if (!inputFile)
         return 1;
-    }
 
+    // Initialize positions and space tracker
     double remaining_space = LINE_WIDTH;
-    float current_Xpos = 0;                     // Initialize X-position tracker
-    float current_Ypos = -5 - 18 * scaleFactor; // Initialize Y-position
+    float current_Xpos = 0, current_Ypos = LINE_SPACING - CHAR_WIDTH * scaleFactor;
 
-    // Read and process each word using fscanf
+    // Process each word from the input file
     char word[100];
-    while (fscanf(file2, "%99s", word) != EOF)
+    while (fscanf(inputFile, "%99s", word) != EOF)
     {
-        // Calculate the width of the current word
-        double wordWidth = word_width(word, scaleFactor);
-
-        // Check if the word fits in the remaining space
-        if (!space_remaining(&remaining_space, wordWidth))
+        double wordWidth = calculate_word_width(word, scaleFactor);
+        if (!fits_in_line(&remaining_space, wordWidth))
         {
-            // Move to a new line
-            current_Xpos = 0;                         // Reset X-position to the start of the line
-            current_Ypos += -5 - 18 * scaleFactor;    // Move down to the next line
-            remaining_space = LINE_WIDTH - wordWidth; // Reset remaining space for the new line
-
-            // Add G-code to move to the new line position
-            char moveToNewLine[100];
-            sprintf(moveToNewLine, "G0 X%.2f Y%.2f\n", current_Xpos, current_Ypos);
-            SendCommands(moveToNewLine);
+            reset_position(&current_Xpos, &current_Ypos, scaleFactor, &remaining_space); // New line
         }
-
-        // Generate G-code for the current word
-        convert_word_to_gcode(word, SingleStrokeData, scaleFactor, &current_Xpos, current_Ypos);
-
-        // Add space after the word
-        current_Xpos += 18 * scaleFactor;    // Increment X-position by the space width
-        remaining_space -= 18 * scaleFactor; // Deduct space width from remaining line space
+        generate_gcode_for_word(word, fontData, scaleFactor, &current_Xpos, current_Ypos); // G-code for word
+        current_Xpos += CHAR_WIDTH * scaleFactor;                                          // Space after the word
+        remaining_space -= CHAR_WIDTH * scaleFactor;
     }
-    // These commands get the robot into 'ready to draw mode' and need to be sent before any writing commands
 
-    sprintf(buffer, "G1 X0 Y0 \n");
-    SendCommands(buffer);
+    // Finish by returning to the origin
+    SendCommands("G1 X0 Y0\n");
+    CloseRS232Port();
+    printf("COM port closed.\n");
+    return 0;
 }
 
-// Send the data to the robot - note in 'PC' mode you need to hit space twice
-// as the dummy 'WaitForReply' has a getch() within the function.
+// Function to send commands to the robot
 void SendCommands(char *buffer)
 {
-    // printf ("Buffer to send: %s", buffer); // For diagnostic purposes only, normally comment out
-    PrintBuffer(&buffer[0]);
-    WaitForReply();
-    Sleep(100); // Can omit this when using the writing robot but has minimal effect
-                // getch(); // Omit this once basic testing with emulator has taken place
+    PrintBuffer(&buffer[0]); // Send buffer to robot
+    WaitForReply();          // Wait for robot acknowledgment
+    Sleep(100);              // Pause briefly
 }
